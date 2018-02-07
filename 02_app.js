52c4ffe5b76316c02cfd8be47c011f40dbdff642
MathieuSchetagne <mathieu1166@me.com>
2018-02-07 11:21:05 -0500

    Bon fonctionnement du fichier json

--- 02_app.js
+++ 02_app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const fs = require('fs');
+
+
 ////////////////////////////////////////////////////////////////Reoute /html/formulaire.html
-app.get('/html/formulaire.html', function (req, res) {
+app.get('/formulaire',  (req, res) => {
  console.log(__dirname);
- res.sendFile( __dirname + "/" + "formulaire.html" );
+ res.sendFile( __dirname + "/public/" + "formulaire.html" );
 })
 
 ///////////////////////////////////////////////////////////////// Route /
@@ -14,9 +17,8 @@ app.get('/', (req, res) => {
 })
 
 ///////////////////////////////////////////Route /traiter_get
-app.get('/traiter_get', function (req, res) {
+app.get('/traiter_get',  (req, res) => {
  // Preparer l'output en format JSON
-
 console.log('la route /traiter_get')
 
 // on utilise l'objet req.query pour récupérer les données GET
@@ -24,13 +26,44 @@ console.log('la route /traiter_get')
  prenom:req.query.prenom,
  nom:req.query.nom,
  telephone:req.query.telephone,
- courriel:req.query.courriel
+ courriel:req.query.courriel,
  };
 console.log(reponse);
  res.end(JSON.stringify(reponse));
+
+ fs.readFile('membres.json', "utf8", (err,data) => {
+    if (err) throw err;
+    let liste = JSON.parse(data)
+    liste.push(reponse);
+    console.log('Sauvegardé');
+    
+    fs.writeFile ("membres.json", JSON.stringify(liste), (err) => {
+        if (err) throw err;
+        });
+
+  });
+
+
 })
 
-var server = app.listen(8081, function () {
+/////////////////////////////////////////////////////////// Route membre
+app.get("/membres", (req,res) => {
+
+    fs.readFile('membres.json', 'utf8', (err, data) => {
+        if (err) throw err;
+       let membres = JSON.parse(data);
+       console.log(membres[1].nom);
+
+       });
+    
+      
+    }
+
+);
+
+
+
+var server = app.listen(8081,  () => {
  var host = server.address().address
  var port = server.address().port
  
